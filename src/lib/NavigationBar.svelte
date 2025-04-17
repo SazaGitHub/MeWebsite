cbafc05e111ab19659372c22dee4f432af83d6a6
saza <tachibanasaza@proton.me>
2025-04-16 23:21:51 -0400

    VERSION IT HAS BEGUN

---
+++ src/lib/NavigationBar.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	import Card from "./Card.svelte";
+
+	let lastScrollY = $state(0);
+	let scrollY = $state(0);
+	let hidden = $state(false);
+</script>
+
+<!-- on scroll up show -->
+<!-- on scroll down hide -->
+<svelte:window
+	on:wheel={(event) => {
+		hidden = event.deltaY > 0;
+	}}
+/>
+
+<nav class={{ hidden }}>
+	<a href="/"><strong>Home</strong></a>
+
+	<a href="/about"><strong>About</strong></a>
+
+	<a href="/calligraphy"><strong>Calligraphy</strong></a>
+
+	<a href="/writing"><strong>Writing</strong></a>
+</nav>
+
+<style>
+	nav {
+		display: flex;
+		background-color: oklch(0.33 0.111 280.06 / 0.6);
+		justify-content: space-around;
+		padding: 1em;
+		outline: none;
+		backdrop-filter: blur(20px);
+
+		position: sticky;
+		top: 0;
+
+		transform: translateY(0);
+		transition-duration: 200ms;
+		box-shadow: 0 0 10px oklch(0.33 0.111 280.06 / 0.6);
+	}
+	nav.hidden {
+		transform: translateY(-100%);
+	}
+	a {
+		color: oklch(0.85 0.0641 267.14);
+		text-decoration: none;
+	}
+</style>
---
+++ src/routes/+error.svelte
@@ -0,0 +1,27 @@
+<script>
+	import Card from "$lib/Card.svelte";
+	import { page } from "$app/state";
+</script>
+
+<svelte:head>
+	<title>Error!</title>
+	{#if page.error != null}
+		<meta name="description" content={page.error.message} />
+	{/if}
+</svelte:head>
+
+<Card>
+	<h1>Error!</h1>
+	{#if page.error != null}
+		<h2>{page.error.message}</h2>
+	{/if}
+</Card>
+
+<style>
+	h1 {
+		font-size: 2rem;
+	}
+	h2 {
+		font-size: 1.5rem;
+	}
+</style>

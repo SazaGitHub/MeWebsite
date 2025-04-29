06e05e67b84ef955a9a4b6e3f27e487625973c3b
saza <tachibanasaza@proton.me>
2025-04-29 10:22:08 -0400

    Some things are fixed

--- src/routes/calligraphy/+page.svelte
+++ src/routes/calligraphy/+page.svelte
@@ -30,10 +30,10 @@
 
 <div>
 	<Card id="calligraphy info">
-		I started calligraphy while in a Japanese college for the first time, and
-		ever since then it has become an important part of my life. I primarily
-		write in what is called 草書 (Sousho), meaning "grass write" which is a
-		highly cursive style.
+		I started calligraphy while in Japan for the first time, and ever since
+		then it has become an important part of my life. I primarily write in what
+		is called 草書 (Sousho), meaning "grass write" which is a highly cursive
+		style.
 	</Card>
 </div>
 
